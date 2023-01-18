cf5766133b3c19234bae17afb0a71715c25377db
Olga <olganedelcuam@gmail.com>
2023-01-18 14:33:48 +0100

    Presentational VS Stateful Components -- example: ExpenseItem and ExpenseForm where one manages a state and the other does not

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -4,22 +4,15 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const Expenseltem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
-
-  const clickHandler = () => {
-    setTitle("Updated"); 
-    console.log(title);
-  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}€</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button>Change Title</button>
     </Card>
   );
 };
