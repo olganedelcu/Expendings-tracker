7cf11e7c46158265df68919e3291ac194acb6b74
Olga <olganedelcuam@gmail.com>
2023-01-18 11:32:13 +0100

    up-todate

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-    const [selectedYear, setSelectedYear] = useState('2020'); // initial value empty string
+    const [selectedYear, setSelectedYear] = useState('2023'); // initial value empty string
 
     const selectedYearHandler = (selectedYear) => {
         setSelectedYear(selectedYear.target.value);
@@ -17,10 +17,10 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select onChange={selectedYearHandler} value={selectedYear}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+          <option value="2022">2023</option>
+          <option value="2021">2022</option>
+          <option value="2020">2021</option>
+          <option value="2019">2020</option>
         </select>
       </div>
     </div>
