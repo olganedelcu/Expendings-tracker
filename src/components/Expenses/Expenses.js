e9d086910a66796bd8b466b0280400fcd8fe2148
Olga <olganedelcuam@gmail.com>
2023-01-23 11:29:31 +0100

    bug expenses.js component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./Expenseltem";
 import Card from "../UI/Card.js";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter.js";
+import ExpensesList from './ExpensesList.js';
+
 
 const NewExpense = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -10,6 +11,9 @@ const NewExpense = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear; // comparing the year to the Selected year
+  });
 
   return (
     <div>
@@ -18,14 +22,7 @@ const NewExpense = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
