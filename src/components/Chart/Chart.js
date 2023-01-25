2e28b1ce02318f563bfc294f3014d8789bc7406c
Olga <olganedelcuam@gmail.com>
2023-01-25 13:23:27 +0100

    Chart component

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = props => {
+    return <div className="chart">
+        <ChartBar/>
+    </div>
+}
+export default Chart;
