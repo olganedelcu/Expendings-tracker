a0c27f224652dadeb0f459dd102621e361484ee2
Olga <olganedelcuam@gmail.com>
2023-01-30 12:00:50 +0100

    styling our Chart and ChartBar components

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './ChartBar.css';
+import "./ChartBar.css";
 
 const ChartBar = (props) => {
-    return <div className="chart-bar">
-        <div className="chart-bar__inner">
-            <div className="chart-bar__fill"></div>
-            <div>
-                <div className="chart-bar__label"></div>
-            </div>
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div className="chart-bar__fill"></div>
+        <div>
+          <div className="chart-bar__label">{props.label}</div>
         </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default ChartBar;
