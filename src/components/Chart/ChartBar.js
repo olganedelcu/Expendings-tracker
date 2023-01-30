28841bab72a47b0dbc96a74bef62945bfca2d4f3
Olga <olganedelcuam@gmail.com>
2023-01-30 11:57:26 +0100

    adding styling to our CharBar componet - nested <div> elements

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 
+import './ChartBar.css';
 
 const ChartBar = (props) => {
-    return <className="chart">
-    {}
-    </>
+    return <div className="chart-bar">
+        <div className="chart-bar__inner">
+            <div className="chart-bar__fill"></div>
+            <div>
+                <div className="chart-bar__label"></div>
+            </div>
+        </div>
+    </div>
 }
 
 export default ChartBar;
