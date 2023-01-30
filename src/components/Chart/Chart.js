08f5a0036e38e8f77ed5ca2b40f5e8c906f2ca85
Olga <olganedelcuam@gmail.com>
2023-01-30 12:07:41 +0100

    in-line styling components

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,12 +4,14 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
-          value={dataPoint.value}
+          value={dataPoint.value} // value in relation with our maxValue to fill it correspondly 
           maxValue={null}
           label={dataPoint.label}
         />
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,10 +3,16 @@ import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
+    let barFillHeight = '0%';
+
+    if(props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div className="chart-bar__fill" style={{height: barFillHeight, b}}></div>
         <div>
           <div className="chart-bar__label">{props.label}</div>
         </div>
