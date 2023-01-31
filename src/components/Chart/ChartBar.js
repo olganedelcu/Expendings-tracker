aeec2305272a4dedf376c64ce4b899709e9d0ff9
Olga <olganedelcuam@gmail.com>
2023-01-31 14:26:57 +0100

    bug in in-line css ChartBar component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -12,7 +12,7 @@ const ChartBar = (props) => {
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{height: barFillHeight, b}}></div>
+        <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
         <div>
           <div className="chart-bar__label">{props.label}</div>
         </div>
