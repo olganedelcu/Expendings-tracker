16eb7b6af262e0f930a92e92710483be1e23227c
Olga <olganedelcuam@gmail.com>
2023-01-31 14:36:50 +0100

    adding all months of the year

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -3,7 +3,20 @@ import React from "react";
 import Chart from "./Chart/Chart.js";
 
 const ExpensesChart = (props) => {
-  const chartDataPoints = [{ label: "Jan", value: 0 }];
+  const chartDataPoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "March", value: 0 },
+    { label: "April", value: 0 },
+    { label: "May", value: 0 },
+    { label: "June", value: 0 },
+    { label: "July", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
 
   return (
     <>
