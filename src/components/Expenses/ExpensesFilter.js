f0315af008a3773b629d492ce07ae038d6736087
olganedelcu <olganedelcuam@gmail.com>
2026-01-03 19:31:34 +0200

    updating years

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -10,10 +10,11 @@ const ExpensesFilter = (props) => {
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value="2022">2023</option>
-          <option value="2021">2022</option>
-          <option value="2020">2021</option>
-          <option value="2019">2020</option>
+          <option value="2022">2026</option>
+          <option value="2022">2025</option>
+          <option value="2021">2024</option>
+          <option value="2020">2023</option>
+          <option value="2019">2022</option>
         </select>
       </div>
     </div>
